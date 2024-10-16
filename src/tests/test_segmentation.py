cfe903cd9e7ba352f026d12f3a6d246bb595c5bb
zouxiaochuan <zouxiaochuan@163.com>
2024-10-16 19:40:30 +0800

    update code

---
+++ src/tests/test_segmentation.py
@@ -0,0 +1,48 @@
+
+import unittest
+import ai_guide.surface
+import open3d as o3d
+import ai_guide.segmentation
+import numpy as np
+
+class TestSegmentation(unittest.TestCase):
+
+    def setUp(self):
+        # Setup code to run before each test
+        pass
+
+    def test_remove_background(self):
+        pcd = o3d.io.read_point_cloud("data/test2.ply")
+        pcd = ai_guide.segmentation.remove_background(pcd)
+        o3d.io.write_point_cloud("data/test2_no_background.ply", pcd)
+        pass
+
+    def test_find_keypoint(self):
+        pcd = o3d.io.read_point_cloud("data/test_no_background.ply")
+        index = ai_guide.segmentation.find_keypoint(pcd)
+
+        # paint color by index
+        colors = np.asarray(pcd.colors)
+        colors[index] = [1, 0, 0]
+        pcd.colors = o3d.utility.Vector3dVector(colors)
+        o3d.io.write_point_cloud("data/test_keypoint.ply", pcd)
+
+        pass
+
+    def test_segment_plane(self):
+        mesh = o3d.io.read_triangle_mesh("data/test2_mesh.ply")
+        points = np.asarray(mesh.vertices)
+        keypoint = ai_guide.segmentation.find_keypoint(points)
+        seg = ai_guide.segmentation.segment_plane(mesh, keypoint)
+
+        # paint color by index
+        o3d.io.write_triangle_mesh("data/test2_seg.ply", seg)
+
+        pass
+
+    def tearDown(self):
+        # Cleanup code to run after each test
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/tests/test_surface.py
@@ -0,0 +1,24 @@
+import unittest
+import ai_guide.surface
+import open3d as o3d
+
+class TestSurface(unittest.TestCase):
+
+    def setUp(self):
+        # Setup code to run before each test
+        pass
+
+    def test_create_surface(self):
+        pcd = o3d.io.read_point_cloud("data/test2_no_background.ply")
+        mesh = ai_guide.surface.create_surface_from_cloud_points(
+            pcd, 0.5
+        )
+        o3d.io.write_triangle_mesh("data/test2_mesh.ply", mesh)
+        pass
+
+    def tearDown(self):
+        # Cleanup code to run after each test
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
